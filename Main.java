bf6ddab3a0db5728e2dc8a386604ef04eb93e461
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-21 22:46:30 +0330

    Modifying the constractor in the book class

--- Main.java
+++ Main.java
@@ -1,3 +1,15 @@
 public class Main {
+    public static void main(String[] args){
+        Library L = new Library();
+        L.addBook(new Book("Java Programing", "Emal Hasanzada", 2025));
+        L.addBook(new Book("C++ Programing", "Mr : Azimi", 2050));
+        L.addBook(new Book("HTML D", "Ahmad", 2013));
+        L.displayAllBooks();
+        L.removeBook(2);
+        L.displayAllBooks();
+        L.searchByTitle("C++ Programing");
+        
+        
+    }
     
 }
