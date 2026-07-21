b0cf16250de656b7ab50670e6bc87cb29cc3323c
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-21 20:47:19 +0330

    Adding the for method of the library class , 1 add , 2 remove, 3 search, 4 display

--- Library.java
+++ Library.java
@@ -1,6 +1,30 @@
 // the class is manages the book collection , 
 // including adding deleting , searching for ,
 //  and displaying books.
+
+import java.util.ArrayList;
+
 public class Library {
+
+        ArrayList<Book > books  = new ArrayList<>();
+    }
+   
+    // Method for adding a book .
+    public void addBook(Book book){
+
+    }
+ 
+    // Method for removed  a book 
+    public void removeBook(int id){
+
+    }
+    // Method for Searched by title a book 
+    public void searchByTitle(String title){
+        
+    }
+    // Method for display all books .
+    public void displayAllBooks(){
+
+    }
     
-}
+
