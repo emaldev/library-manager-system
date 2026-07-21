31a5a3db9705695bde919dea661f2024779f0f1c
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-22 00:04:47 +0330

    Adding savaToFiel in filemanager class

--- FileManager.java
+++ FileManager.java
@@ -3,5 +3,13 @@
 
 import java.util.*;
 public class FileManager {
+
+    public static void  savaToFile(String fileName, Library library){
+        // 1. Get the books from the library
+    // 2. Open the file using FileWriter
+    // 3. Use a loop to write each book to the file in the format "id,title,author,year"
+    // 4. Handle errors using try-catch
+    }
     
+
 }
