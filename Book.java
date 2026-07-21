aec1c825661262889200f290f7bcd6ec913e253d
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-21 20:27:26 +0330

    Create constracot for book class

--- Book.java
+++ Book.java
@@ -1,5 +1,19 @@
 public class Book {
     
+    private String title;
+    private String author;
+    private int year; 
+    private int id;
+    private static int counter = 1; // To generate an automatic ID
 
-    
+    // Creating a counstractor 
+    public  Book(String title , String author, int year, int id){
+          this.title = title;
+          this.author = author;
+          this.year = year;
+          this.id = counter++;
+          
+    }
+
+
 }
