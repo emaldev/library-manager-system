f6bd27ba69464ccccf5b6ada6804c84396a36505
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-23 18:26:09 +0330

    Adding the menu of the magener ane switch added book

--- Main.java
+++ Main.java
@@ -1,14 +1,56 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args){
-        Library L = new Library();
-        L.addBook(new Book("Java Programing", "Emal Hasanzada", 2025));
-        L.addBook(new Book("C++ Programing", "Mr : Azimi", 2050));
-        L.addBook(new Book("HTML D", "Ahmad", 2013));
-        L.displayAllBooks();
-        L.removeBook(2);
-        L.displayAllBooks();
-        L.searchByTitle("C++ Programing");
-        
+        Scanner input = new Scanner(System.in);
+        Library library = new Library();
+        int choice;
+      
+        do{
+            System.out.println("\n ==== Library Mabager System ===");
+            System.out.println("1. Add Book");
+            System.out.println("2. Remove Book");
+            System.out.println("3. Search Book");
+            System.out.println("4. Display All Books");
+            System.out.println("5. Save to File");
+            System.out.println("6. Load from File");
+            System.out.println("7 Exit");
+            System.out.println("Enter your choice: ");
+            
+            choice  =input.nextInt();
+            input.nextLine(); // enter buffoon
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter title :");
+                    String title = input.nextLine();
+                    System.out.println("Enter Author: ");
+                    String author = input.nextLine();
+                    System.out.println("Enter Year: ");
+                    int year = input.nextInt();
+                    input.nextLine();
+                    library.addBook(new Book(title, author, year));
+                    System.out.println("Book added succefully!");
+                   
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+
+
+            
+                default:
+                    break;
+            }
+        }
         
     }
     
