2f4e65ce734862f882ead3707c426760cf8b0028
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-23 23:38:35 +0330

    Add ActionListener for abb book

--- LibraryGUI.java
+++ LibraryGUI.java
@@ -1,6 +1,11 @@
+import java.awt.Color;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 import javax.swing.JPanel;
+
 
 public class LibraryGUI {
     public static void main(String[] args){
@@ -17,7 +22,10 @@ public class LibraryGUI {
         JButton displayButton = new JButton("Display Books");
         JButton saveButton = new JButton("Save File");
         JButton loadButton = new JButton("Load File");
+        JButton exiButton = new JButton("Exit");
 
+     
+      
         // Add buttom to panel
         JPanel panel = new JPanel();
         panel.add(addButton);
@@ -26,9 +34,52 @@ public class LibraryGUI {
         panel.add(displayButton);
         panel.add(saveButton);
         panel.add(loadButton);
+        panel.add(exiButton);
+        addButton.addActionListener(e -> {
+       
+       
+        JLabel titLabel = new JLabel("Title");
+        JTextField titleField = new JTextField(20);
+       
+       
+        // for title 
+        JFrame addframe = new JFrame("Add Book");
+        JButton titleButton = new JButton("Add");
+        JPanel addPanel = new JPanel();
+           // for Author 
+        JLabel authorLabel = new JLabel("Author:");
+        JTextField authorField = new JTextField(20);
+        // for Years 
+        JLabel yearLabel = new JLabel("Year:");
+        JTextField yearField = new JTextField(20);
+
+
+        addPanel.add(titLabel);
+        addPanel.add(titleField);
+       
+        addPanel.add(authorLabel);
+        addPanel.add(authorField);
+        addPanel.add(yearLabel);
+        addPanel.add(yearField);
+        addPanel.add(titleButton);
+       
+        addframe.add(addPanel);
+    
+
+        addframe.setSize(600, 300);
+        addframe.setVisible(true);
+                
+      
+       });
+        
+
+        panel.setBackground(Color.LIGHT_GRAY);
+       
+
 
         // Add panel to frame
         frame.add(panel);
+        
         frame.setVisible(true);
 
     }
