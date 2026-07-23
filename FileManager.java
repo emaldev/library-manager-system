452de208578dc8fc40198f5ffeb495307f847743
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-23 18:13:47 +0330

    Adding the Library in the file loadfrom

--- FileManager.java
+++ FileManager.java
@@ -9,7 +9,7 @@ import java.io.FileReader;
 import java.io.IOException;
 public class FileManager {
 
-    public static void  savaToFile(String fileName, Library library){
+    public static void  saveToFile(String fileName, Library library){
         // 1. Get the books from the library
     // 2. Open the file using FileWriter
     // 3. Use a loop to write each book to the file in the format "id,title,author,year"
@@ -27,15 +27,37 @@ public class FileManager {
 
           
     }
-        public static void loadFromFile(String fileName){
+        public static Library loadFromFile(String fileName){
         // 1. Create a new Library
     // 2. Read the file using Scanner
     // 3. Split each line using split(",")
     // 4. Create a new Book with id, title, author, and year
     // 5. Add the book to the Library
     // 6. Return the Library
+        Library library = new Library();
+        try(Scanner input = new Scanner(new File(fileName))){
+            while(input.hasNextLine()){
+                String line = input.nextLine();
+                String[] parts = line.split(",");
+                int id = Integer.parseInt(parts[0].trim());
+                String title = parts[1].trim();
+                String author = parts[2].trim();
+                int year = Integer.parseInt(parts[3].trim());
 
+                library.addBook(new Book(title, author, year));
+             
+
+            }
+
+        }catch(IOException e){
+            System.out.println("Error loading data: " + e.getMessage());
+        }
+        return library;
+        
+    }
+    
+   
     }
     
 
-}
+
