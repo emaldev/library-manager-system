aa7e3ce0932f7c42285ef74b8f0a74524d76fca0
emaldev <261611436+emaldev@users.noreply.github.com>
2026-07-23 18:39:05 +0330

    Adding option 2 through 5 in main

--- Main.java
+++ Main.java
@@ -33,12 +33,23 @@ public class Main {
                    
                     break;
                 case 2:
+                    System.out.println("Enter the book's ID for delete it: ");
+                    int id = input.nextInt();
+                    library.removeBook(id);
                     break;
                 case 3:
+                    System.out.println("Enter the title to search for a book: ");
+                    title =input.nextLine();
+                    library.searchByTitle(title);
+
                     break;
                 case 4:
+                    library.displayAllBooks();
                     break;
                 case 5:
+                    System.out.println("Enter file name to save: ");
+                    String fileName = input.nextLine();
+                    FileManager.saveToFile(fileName, library);
                     break;
                 case 6:
                     break;
